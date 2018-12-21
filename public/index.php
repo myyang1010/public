c7a20e728883983da4d5dc2239241c627e1c8d88
myyang <my254@njit.edu>
2018-12-21 17:16:00 -0500

    Feature: Using a Factory (In Progress)

--- public/index.php
+++ public/index.php
@@ -6,5 +6,71 @@
  * Time: 12:47 PM
  */
 
-echo 'Hello World';
+main::start();
 
+class main {
+
+    static public function start() {
+        $records = csv::getrecords();
+        $table = html::generatetable($records);
+        $table = 'test';
+        system::printpage($table);
+    }
+
+}
+
+class csv {
+
+    static public function getrecords() {
+
+        $make = 'Ford';
+        $model = 'Taurus';
+        $car = AutomobileFactory::create($make, $model);
+        $records[] = $car;
+        print_r($records);
+
+        return $records;
+    }
+}
+
+class html {
+
+    static public function generatetable() {
+
+        $table = $records;
+        return $table;
+    }
+}
+
+class system {
+
+    static public function printpage($page) {
+
+        echo 'test';
+    }
+}
+
+class Automobile
+{
+    private $vehicleMake;
+    private $vehicleModel;
+
+    public function __construct($make, $model)
+    {
+        $this->vehicleMake = $make;
+        $this->vehicleModel = $model;
+    }
+
+    public function getMakeAndModel()
+    {
+        return $this->vehicleMake . ' ' . $this->vehicleModel;
+    }
+}
+
+class AutomobileFactory
+{
+    public static function create($make, $model)
+    {
+        return new Automobile($make, $model);
+    }
+}
